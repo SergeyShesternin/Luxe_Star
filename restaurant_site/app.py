6415c10fc16046c5a13bc7b90bbb4bf3be43e2bf
Bolkich <abakumovs705@gmail.com>
2025-05-15 21:24:00 +0300

    Add files via upload
    ОПЯТЬ ГИФКА НЕ ГРУЗИТ МНЕ ПИШИТЕ Я СКИНУ) НЕДОРАБОТАННАЯ ВЕРСИЯ ЗА ТО СДЕЛАЛ ГЛ ЭКРАН И ЧУТ МЕНЮ

--- restaurant_site/app.py
+++ restaurant_site/app.py
@@ -10,5 +10,13 @@ def home():
 def menu():
     return render_template('menu.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
