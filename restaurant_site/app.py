d2e9ec76097ede71df2d5f6a951ad826fc2e7abe
Bolkich <abakumovs705@gmail.com>
2025-05-17 00:28:40 +0300

    Add files via upload
    УРА Я СДЕЛАЛ ОСНОВНУЮ ЧАСТЬ 🤯🤯🤯🤯🤯🤯🤯🤯🤯

--- restaurant_site/app.py
+++ restaurant_site/app.py
@@ -1,22 +1,24 @@
 from flask import Flask, render_template
+#импорт библиотек
 
-app = Flask(__name__)
+app = Flask(__name__) #запускаем фласк
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') #главная страница
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    return render_template('menu.html') #меню
 
 @app.route('/services')
 def services():
-    return render_template('services.html')
+    return render_template('services.html') #услуги
 
 @app.route('/contacts')
 def contacts():
-    return render_template('contacts.html')
+    return render_template('contacts.html') #контакты
 
 if __name__ == '__main__':
     app.run(debug=True)
+    #конец
